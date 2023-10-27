68d4b5179c346ba22dbaa945c440bf990f408e1d
dontexit <edwinhamal@gmail.com>
2023-10-27 17:59:50 +0545

    add register and buffer keymaps for telescope

--- lua/winde/packer.lua
+++ lua/winde/packer.lua
@@ -17,10 +17,11 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+        requires = { {'nvim-lua/plenary.nvim'} }
+        }
+    
     use('nvim-treesitter/nvim-treesitter',
         { run = ':TSUpdate' })
     use 'nvim-treesitter/playground'
